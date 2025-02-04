632d90de2354c1083235c5b8a9033eb1e2d2d358
Yayodelariva <eduardodlrp@outlook.com>
2025-02-04 17:10:31 -0600

    fix typo

--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,11 @@ module.exports = {
   },
   devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./src/template.html"],
+    watchFiles: ["./src/index.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      template: "./src/index.html",
     }),
   ],
   module: {
